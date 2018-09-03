1705ab5321812ce2ab45bbfa291c13269c6e8228
Build Server <oir.robot@directum.ru>
2018-09-03 10:32:48 +0400

    Initial Commit

---
+++ Test4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test4
+//
+//  Created by Шишов Дмитрий (Shishov_DR) on 03.09.2018.
+//  Copyright © 2018 Шишов Дмитрий (Shishov_DR). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
